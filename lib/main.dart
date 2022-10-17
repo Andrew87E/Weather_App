24d5ab36f426f4a0b187d543542a3aaf1e9e8514
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-10-17 12:02:59 -0500

    styling

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,28 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String btnName = 'Click Me';
+  int value = 0;
+  Color btnColor = Colors.black;
+
+  void changeBtnColor() {
+    if (btnColor == Colors.black) {
+      btnColor = Color.fromARGB(255, 10, 191, 227);
+    } else {
+      btnColor = const Color.fromARGB(255, 0, 0, 0);
+    }
+  }
 
   void btnPress() {
     if (btnName == 'Click Me') {
       setState(() {
         btnName = 'Clicked';
       });
+      changeBtnColor();
     } else {
       setState(() {
         btnName = 'Click Me';
       });
+      changeBtnColor();
     }
   }
 
@@ -36,7 +48,24 @@ class _MyAppState extends State<MyApp> {
           child: Text('My First App'),
         )),
         body: Center(
-            child: ElevatedButton(onPressed: btnPress, child: Text(btnName))),
+            child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ElevatedButton(
+                  onPressed: btnPress,
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.red,
+                    backgroundColor: btnColor,
+                  ),
+                  child: Text(btnName)),
+              ElevatedButton(onPressed: btnPress, child: Text(btnName)),
+            ],
+          ),
+        )),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
             BottomNavigationBarItem(
@@ -48,6 +77,12 @@ class _MyAppState extends State<MyApp> {
               label: 'Settings',
             ),
           ],
+          currentIndex: value,
+          onTap: (int value) {
+            setState(() {
+              value = value;
+            });
+          },
         ),
       ),
     );
