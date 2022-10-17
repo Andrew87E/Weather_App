3cc36fc092032ab8eeb4eb02c3f9c267c8e827e5
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-10-17 11:04:07 -0500

    basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({ Key? key }) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  String btnName = 'Click Me';
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My First App'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+            title: const Center(
+          child: Text('My First App'),
+        )),
+        body: Center(
+            child: ElevatedButton(onPressed: () {}, child: Text(btnName))),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
         ),
       ),
-      );
+    );
   }
 }
