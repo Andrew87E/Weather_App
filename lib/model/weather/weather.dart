e97e28a9f7e87ac9d79dad9862bf6bac9642a2bb
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-10-18 13:14:26 -0500

    model fix

--- lib/model/weather/weather.dart
+++ lib/model/weather/weather.dart
@@ -1,69 +1,27 @@
-import 'package:collection/collection.dart';
-
-import 'current.dart';
-import 'daily.dart';
-import 'hourly.dart';
-
 class Weather {
-  double? lat;
-  double? lon;
-  String? timezone;
-  int? timezoneOffset;
-  Current? current;
-  List<Hourly>? hourly;
-  List<Daily>? daily;
+  int? id;
+  String? main;
+  String? description;
+  String? icon;
 
   Weather({
-    this.lat,
-    this.lon,
-    this.timezone,
-    this.timezoneOffset,
-    this.current,
-    this.hourly,
-    this.daily,
+    this.id,
+    this.main,
+    this.description,
+    this.icon,
   });
 
   factory Weather.fromJson(Map<String, dynamic> json) => Weather(
-        lat: (json['lat'] as num?)?.toDouble(),
-        lon: (json['lon'] as num?)?.toDouble(),
-        timezone: json['timezone'] as String?,
-        timezoneOffset: json['timezone_offset'] as int?,
-        current: json['current'] == null
-            ? null
-            : Current.fromJson(json['current'] as Map<String, dynamic>),
-        hourly: (json['hourly'] as List<dynamic>?)
-            ?.map((e) => Hourly.fromJson(e as Map<String, dynamic>))
-            .toList(),
-        daily: (json['daily'] as List<dynamic>?)
-            ?.map((e) => Daily.fromJson(e as Map<String, dynamic>))
-            .toList(),
+        id: json['id'] as int?,
+        main: json['main'] as String?,
+        description: json['description'] as String?,
+        icon: json['icon'] as String?,
       );
 
   Map<String, dynamic> toJson() => {
-        'lat': lat,
-        'lon': lon,
-        'timezone': timezone,
-        'timezone_offset': timezoneOffset,
-        'current': current?.toJson(),
-        'hourly': hourly?.map((e) => e.toJson()).toList(),
-        'daily': daily?.map((e) => e.toJson()).toList(),
+        'id': id,
+        'main': main,
+        'description': description,
+        'icon': icon,
       };
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(other, this)) return true;
-    if (other is! Weather) return false;
-    final mapEquals = const DeepCollectionEquality().equals;
-    return mapEquals(other.toJson(), toJson());
-  }
-
-  @override
-  int get hashCode =>
-      lat.hashCode ^
-      lon.hashCode ^
-      timezone.hashCode ^
-      timezoneOffset.hashCode ^
-      current.hashCode ^
-      hourly.hashCode ^
-      daily.hashCode;
 }
