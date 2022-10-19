08656792340ef599882d1ce6e00d818578120808
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-10-19 12:48:37 -0500

    home page

--- lib/utils/api_url.dart
+++ lib/utils/api_url.dart
@@ -4,7 +4,7 @@ String apiURL(var lat, var lon) {
   String url;
 
   url =
-      "https://api.openweathermap.org/data/3.0/onecall?lat=37.39454173713587&lon=-122.0798862385229&units=imperial&appid=589d1a71ed61a2bc78e01fd58646b37a&exclude=minutely";
+      "https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&units=imperial&appid=$apiKey&exclude=minutely";
 
   return url;
 }
